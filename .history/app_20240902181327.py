c00773e050914203439a9a79e8e3d78e70bc031c
leitliniengpt <leitliniengpt@gmail.com>
2024-09-02 18:23:46 +0200

    divided the FlaskGPT.py into two applications (app.py and langchain_logic.py) --> easier to build in original functionality from our app.

---
+++ .history/app_20240902181327.py
@@ -0,0 +1,51 @@
+import json
+import time
+import threading
+from flask import Flask, Response, request, send_from_directory
+from flask_cors import CORS
+
+# Import the necessary variables and classes from langchain_logic.py
+from langchain_logic import prompt_queue, sse_event_queue, StreamHandler, qa_chain, response_thread
+
+app = Flask(__name__, static_folder='./my-app/build', static_url_path='/')
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Enable CORS for all routes and allow requests from React frontend
+
+@app.route('/')
+def serve_react_app():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    prompt = request.json.get('question')
+    if prompt:
+        prompt_queue.put(prompt)
+        return {'status': 'success'}, 200
+    return {'status': 'failed', 'message': 'No prompt provided.'}, 400
+
+def send_sse_data():
+    global qa_chain, prompt_queue, sse_event_queue, response_thread
+    while True:
+        if not prompt_queue.empty():
+            if response_thread and response_thread.is_alive():
+                continue
+
+            prompt = prompt_queue.get()
+
+            response_thread = threading.Thread(target=qa_chain.run, args=(prompt,), kwargs={'callbacks': [StreamHandler()]})
+            response_thread.start()
+
+        while not sse_event_queue.empty():
+            sse_event = sse_event_queue.get()
+            yield f"data: {json.dumps(sse_event)}\n\n"
+
+        time.sleep(1)
+
+@app.route('/stream', methods=['GET'])
+def stream():
+    def event_stream():
+        return send_sse_data()
+
+    return Response(event_stream(), content_type='text/event-stream')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8080)
